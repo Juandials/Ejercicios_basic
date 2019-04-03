c0c969194d53bff2dce8eb48bbd5ffa37aa7f12e
Juandials <judilemo01@gmail.com>
2019-04-03 11:47:31 -0500

    Update

--- src/ejercicios/basicos/Ejercicio12.java
+++ src/ejercicios/basicos/Ejercicio12.java
@@ -13,11 +13,11 @@ public class Ejercicio12 {
     public void operacion (){
     float a,b,c;
     Scanner sc = new Scanner (System.in);
-        System.out.println("Ingrese el valor de a");
+        System.out.println("Ingrese el valor de a:");
     a=sc.nextFloat();
-     System.out.println("Ingrese el valor de b");
+     System.out.println("Ingrese el valor de b:");
     b=sc.nextFloat();
-     System.out.println("Ingrese el valor de c");
+     System.out.println("Ingrese el valor de c:");
     c=sc.nextFloat();
         System.out.println(((a+7*c)/(b+2-a)+2*b));
         }        
--- src/ejercicios/basicos/Ejercicio14.java
+++ src/ejercicios/basicos/Ejercicio14.java
@@ -4,11 +4,21 @@
  * and open the template in the editor.
  */
 package ejercicios.basicos;
-
+import java.util.Scanner;
 /**
  *
  * @author Estudiantes
  */
 public class Ejercicio14 {
-    
+     public void operacion (){
+    float a,b,c;
+    Scanner sc = new Scanner (System.in);
+        System.out.println("Ingrese el valor de a:");
+    a=sc.nextFloat();
+     System.out.println("Ingrese el valor de b:");
+    b=sc.nextFloat();
+     System.out.println("Ingrese el valor de c:");
+    c=sc.nextFloat();
+        System.out.println((-b+(Math.sqrt(Math.pow(b,2)))-4*a*c)/(2*a));
+        }  
 }
